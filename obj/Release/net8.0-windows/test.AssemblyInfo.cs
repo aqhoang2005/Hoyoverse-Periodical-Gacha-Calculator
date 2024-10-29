94e66b9a6dbc6a769226b7802e7fee9ee1ffc262
Austin Hoang <neo322442@gmail.com>
2024-10-29 13:22:13 -0400

    Changed Icon and Added On-Hand Calculating

--- obj/Release/net8.0-windows/test.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/test.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5abb2ee5b8655fde20bb6745591e8015807ee271")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
